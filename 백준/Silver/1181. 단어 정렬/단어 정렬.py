60138e646d7e1457c6185439b6ceca08bf461af3
MINHEE KANG <86910692+minikryc@users.noreply.github.com>
2025-08-30 13:14:09 +0900

    [Silver V] Title: 단어 정렬, Time: 68 ms, Memory: 39508 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,13 +1,17 @@
 import sys
 
+# 한줄씩 불러오는 input() 재정의
 input = sys.stdin.readline
-N = int(input().strip())
-words = set()  # 중복 제거
+# 입력 뒤 \n 제거
+n = int(input().strip())
+# set 선언 (list말고 set로 해야 중복 제거됨)
+words = set()
 
-for _ in range(N):
+# words라는 set에 한 요소씩 추가
+for i in range(n):
     words.add(input().strip())
 
-# 길이 기준 정렬, 같은 길이면 사전순 정렬
+# len(w) 길이 우선 정렬, 다음 사전정의 기준 정렬
 sorted_words = sorted(words, key=lambda w: (len(w), w))
 
 print("\n".join(sorted_words))
